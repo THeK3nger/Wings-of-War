7e5fdc38a5c5082c2ee7faa07fc396279e47066a
IronMan <thek3nger@gmail.com>
2012-05-07 18:30:05 +0200

    Add: NetBeans Project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: davide
+ *
+ * Created on May 7, 2012, 6:26 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
