591422e6c5ad15feee782b4a9a1f6ba36293e2b5
Hulk <fabrizio@MACchia.(none)>
2012-05-07 19:23:39 +0200

    Curiosissimo di vedere se funzionano... sarebbe una sorpresa

--- World.cpp
+++ World.cpp
@@ -1,18 +1,31 @@
 /* 
  * File:   World.cpp
- * Author: davide
+ * Author: Iron Man, Thor, Hulk
  * 
  * Created on May 7, 2012, 6:45 PM
  */
 
 #include "World.h"
 
-World::World() {
-}
-
-World::World(const World& orig) {
+World::World(float w, float h, std::vector<Plane> p) {
+    this->width = w;
+    this->height = h;
+    
+    for (int i=0; i<p.size(); i++){
+        this->planes.push_back(p[i]);
+    }
 }
 
 World::~World() {
 }
 
+void World::draw(){
+    
+}
+
+Plane* World::GetAereoById(int id){
+    for (int i=0; i<this->planes.size(); i++){
+        if(this->planes[i].id == id)
+            return &(this->planes[i]);
+    }
+}
--- World.h
+++ World.h
@@ -1,21 +1,26 @@
 /* 
  * File:   World.h
- * Author: davide
+ * Author: Iron Man, Thor, Hulk
  *
  * Created on May 7, 2012, 6:45 PM
  */
 
+#include "Plane.h"
+#include <vector>
+
 #ifndef WORLD_H
 #define	WORLD_H
 
-class World {
-public:
-    World();
-    World(const World& orig);
-    virtual ~World();
-private:
+class World{
+    float width;
+    float height;
+    std::vector<Plane> planes;
 
+        public:
+        World(float w, float h, std::vector<Plane> p);
+        ~World();
+        void draw();
+        Plane* GetAereoById(int id); // return pointer of the plane
 };
 
 #endif	/* WORLD_H */
-
