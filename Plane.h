ffc1df3875c9d2fd2df5fb13b0ac4a6835ac794b
Thor <maurilio.dicicco@gmail.com>
2012-05-07 19:25:49 +0200

    header created

--- Plane.h
+++ Plane.h
@@ -4,17 +4,42 @@
  *
  * Created on May 7, 2012, 6:44 PM
  */
-
 #ifndef PLANE_H
 #define	PLANE_H
 
+#include "Card.h"
+
 class Plane {
 public:
+    
     Plane();
     Plane(const Plane& orig);
     virtual ~Plane();
+    
+    void move(Card card);
+    int getDamage();
+    int getHealth();
+    
+    void setDamage(int value);
+    
+    void getPosition(float* thePosition);
+    
+    int getLastMove();
+    
+    bool moveIsValid(Card card);
+    
 private:
-
+       
+    float posx;
+    float posy;
+    float theta;
+    
+    int id;
+    
+    int damage;
+    int health;
+    Card::CType lastmove;
+    
 };
 
 #endif	/* PLANE_H */
