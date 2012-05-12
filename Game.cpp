db04693807e2e036b3245cf8fd6671fb435451df
Thor <maurilio.dicicco@gmail.com>
2012-05-13 00:06:30 +0200

    instantiation of the render window, basically draws a fancy red square

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,38 @@
 #include "Game.h"
-
-Game::Game() {
+ /*!
+ * Constructor:
+ * sets the size and the depth of the window
+ * sets the actual state
+ */
+Game::Game(int h, int w, int d) 
+{
+  width=w;
+  height=h;
+  depth=d;
+  //name=n;
+  
+  _gameState = Game::Uninitialized;
+    
+}
+ /*!
+  * Game initialization routine != from the alloc/init of the constructor
+ */
+void Game::init()
+{
+    if(_gameState!=Game::Uninitialized) return;
+    
+    _mainWindow.Create(sf::VideoMode(width,height,depth),"Wings of War");
+    _gameState = Game::Playing;
+}
+    
+    
+/*!
+ * Drawing loop routine
+ */
+void Game::run()
+{  
+        _mainWindow.Clear(sf::Color(255,0,0));
+        _mainWindow.Display();
 }
 
 Game::Game(const Game& orig) {
--- Game.h
+++ Game.h
@@ -5,6 +5,10 @@
 #include "World.h"
 #include "WoWBrain.h"
 
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+
+
 /*!
  * \brief This class is the core class of the Game.
  * 
@@ -12,12 +16,62 @@
  */
 class Game {
 public:
-    Game();
+    
+    /*!
+     * Costructor for the class Game.
+     * Basically here we define all the parameters to instantiate a window 
+     * \param int   h=height
+     * \param int   w=width
+     * \param int   d=depth
+     * \param char* n=name
+     */
+    
+    Game(int h, int w, int d);
+    
     Game(const Game& orig);
+    
     virtual ~Game();
+    
+    
+    /*!
+     * Game initialization routine != from the alloc/init of the constructor
+     */
+    void init();
+    
+    
+    /*!
+     * Main loop for the game
+     */
+    void run();
+    
+    
 private:
     World* world;
     WoWBrain* ai;
+    
+    /*!
+     * Window height*width*color_depth + name
+     */
+    int height;
+    int width;
+    int depth;
+    char name[100];
+    
+    /*!
+     * Possible states of the game
+     */
+    enum GameState { Uninitialized, ShowingSplash, Paused, ShowingMenu, Playing, Exiting };
+    
+    /*!
+     * Actual state of the game
+     */
+ 
+     GameState _gameState;
+    
+    /*!
+     * Render window
+     */
+     sf::RenderWindow _mainWindow;
 };
 
 #endif	/* GAME_H */
