2be7862af25bcb9f96d1a0879e37b78710b24e78
IronMan <thek3nger@gmail.com>
2012-05-16 19:28:13 +0200

    Just a make up editing...

--- World.cpp
+++ World.cpp
@@ -9,19 +9,19 @@ World::World(float w, float h) {
 World::~World() {
 }
 
-void World::draw(){
-    
+void World::draw() {
+
 }
 
-Plane* World::GetPlaneById(int id){
-    for (int i=0; i<this->planes.size(); i++){
-        if(this->planes[i]->getId() == id)
+Plane* World::GetPlaneById(int id) {
+    for (int i = 0; i<this->planes.size(); i++) {
+        if (this->planes[i]->getId() == id)
             return this->planes[i];
     }
     return 0;
 }
 
-void World::addPlane(Plane* plane){
+void World::addPlane(Plane* plane) {
     this->planes.push_back(plane);
 }
 
@@ -33,6 +33,6 @@ float World::getWidth() const {
     return width;
 }
 
-std::vector<Plane*> * World::getPlanes(){
+std::vector<Plane*> * World::getPlanes() {
     return &this->planes;
 }
