a9cdf5bc7303a1b234869d2148e762ba0580d35a
IronMan <thek3nger@gmail.com>
2012-10-19 13:25:37 +0200

    Ops. Error.

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -5,7 +5,7 @@
 
 Animation::Animation(float xi, float yi, float thetai, float xf, float yf, float thetaf) :
     init_x(xi), init_y(yi), init_theta(thetai),
-    final_x(xf), final_y(xf), final_theta(thetaf)
+    final_x(xf), final_y(yf), final_theta(thetaf)
 {
     // TODO: compute k according to the given values
     this->k = 50;
